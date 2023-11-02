74b98fc638507cef6ad252e04f4c260ed2570c40
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-11-02 18:40:33 +0200

    Fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-
+A simple webpage which features the "Masonry Layout". 
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 
 
 export default defineConfig({
-  plugins: [
-    react,
-  ],
-  base: "/masonry-gallery",
+  plugins: [react()],
+  base: '/masonry-gallery',
 });
